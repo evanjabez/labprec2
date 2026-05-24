4d7c432faa096c274f463b10502bc68658047ac8
Evan <EVANJABEZ.25@ichat.sp.edu.sg>
2026-05-24 21:32:54 +0800

    with print f

--- bmiprec.py
+++ bmiprec.py
@@ -5,12 +5,12 @@ def calculate_bmi(height, weight):
     bmi = weight / (height * height)
     
     if bmi < 18.5:
-        print("BMI =" + str(bmi) + " (Underweight)")
+        print(f"BMI ={bmi} (Underweight)")
         
     elif bmi < 25:
-        print("BMI = " + str(bmi) + " (Normal weight)")
+        print(f"BMI = {bmi} (Normal weight)")
 
     else:
-        print("BMI = " + str(bmi) + " (Overweight)")
+        print(f"BMI = {bmi} (Overweight)")
 
 calculate_bmi(1.73, 57)
